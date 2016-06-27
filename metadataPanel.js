3a16dc9e51167a768da1272fc4cff316f47d7c7b
JaZahn <jcleveng@fas.harvard.edu>
2016-06-27 14:18:22 -0400

    this is the first functional version -- using eventEmitter

--- metadataPanel.js
+++ metadataPanel.js
@@ -1,67 +1,39 @@
 (function(M) {
 
-  M.metadataPlugin = function(){
-    console.log("metadataPlugin1");
-    var that = this;
+  M.prototype.metadataPlugin = function(){
+    var mirador = this;
 
-    $(document).ready(function(){
+    mirador.eventEmitter.subscribe('WINDOW_ELEMENT_UPDATED', function(event, data){
+      var windowId = data.windowId;
 
-      $windowTitle = $(window.mirador.viewer.element).has('.window-manifest-title');
-      console.log($windowTitle);
-      console.log($windowTitle.html());
-
-
-      var $sidePanel = $('.content-container .sidePanel');
-      console.log(window.mirador.viewer);
-      console.log($(window.mirador.viewer.element).find('.window-manifest-title'));
-
-
-      // get the data
-      var manifestUri = window.mirador.viewer.data[0].manifestUri;
-      var datas = [];
+      // get the metadata
+      var manifestUri = mirador.viewer.data[0].manifestUri;
+      var metadatas = [];
       $.getJSON(manifestUri, function(data) {
-        //console.log(data);
-        // an array of data
-        //console.log(data.sequences[0].canvases);
-        datas = data.sequences[0].canvases;
-
+        metadatas = data.sequences[0].canvases;
       });
 
-      // on an event, populate the sidePanel
-      // what's the event?
-      //var imageView = $('.image-view')[0];
-      var viewContainer = $('.content-container');
-      console.log(viewContainer);
+      mirador.eventEmitter.subscribe('currentCanvasIDUpdated.' + windowId, function(event, data){
+        var imageId = data;
 
-      var observer = new MutationObserver(function(mutations){
-        mutations.forEach(function(mutation){
-          console.log(mutation.type);
+        var $sidePanel = $('.sidePanel .tabContentArea');
+        metadatas.forEach(function(item){
+          if(item['@id'] == imageId){
+            //item.label
+            var html = '<b>' + item.label + '</b><br/>';
+            if(item.metadata){
+              item.metadata.forEach(function(md){
+                //md.label
+                //md.value
+                md.value = md.value || "No value";
+                html += md.label + ': ' + md.value + '<br/>';
+              });
+            }
+            $sidePanel.html(html);
+          }
         });
+
       });
-
-      observer.observe(viewContainer, {attributes: true, childList: true, characterData: true});
-
-
-      // get the id of the current image...
-
-      console.log(window.mirador);
-      var ee = window.mirador.eventEmitter;
-      window.mirador.eventEmitter.subscribe(('currentCanvasIDUpdated.' + window.mirador.windowId), function(event) {
-        console.log("changed...");
-      });
-
-
-
-      /*
-      var mirador = window.mirador;
-      console.log(mirador);
-      console.log(mirador.viewer.element);
-      $windowTitle = jQuery(mirador.viewer.element).find('.window-manifest-title');
-      console.log($windowTitle);
-      console.log($windowTitle.html());
-      */
-      //jQuery('.window-manifest-title').html("asdf");
-
     });
 
 
